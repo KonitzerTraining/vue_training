def43bdb1c6a7862b09fe8c90fc3aa25b167be62
KonitzerTraining <mk@konitzer-training.de>
2020-03-18 10:00:56 +0100

    Vue Instance

--- 01_vue_als_widgets/02_binding/app/main.js
+++ 01_vue_als_widgets/02_binding/app/main.js
@@ -1,4 +1,14 @@
 // neue Vue-Instanz
 let app = new Vue({ // Konfigurations-Objekt
-    el: '#crm' // Beliebiger CSS-Selector
+    el: '#crm', // Beliebiger CSS-Selector
+    data: {
+        version: '0.0.1',
+        status: 234, // 1 - 5
+        customers: [
+            {
+                id: 1,
+                name: 'Hans'
+            }
+        ]
+    }
 });
