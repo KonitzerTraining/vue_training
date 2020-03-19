84e1047c34c699b896d7e01047445ed34170d516
KonitzerTraining <mk@konitzer-training.de>
2020-03-19 09:58:53 +0100

    Vue Instance

--- 02_vue_cli/little-crm-2/src/router/index.js
+++ 02_vue_cli/little-crm-2/src/router/index.js
@@ -11,6 +11,12 @@ const routes = [
     component: Home
   },
   {
+    path: '/imprint',
+    name: 'Imprint',
+    // Lazy Loading
+    component: () => import('../views/Imprint.vue')
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
---
+++ 02_vue_cli/little-crm-2/src/views/Imprint.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>Imprint</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Imprint'
+}
+</script>
+
+<style scoped>
+
+</style>
