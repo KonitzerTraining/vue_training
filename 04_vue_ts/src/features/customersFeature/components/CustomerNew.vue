f939713b3435fe2959c29afe9ca5a9d33903dfbd
KonitzerTraining <mk@konitzer-training.de>
2020-03-20 14:58:56 +0100

    Vue Instance

--- 04_vue_ts/src/features/customersFeature/components/CustomerNew.vue
+++ 04_vue_ts/src/features/customersFeature/components/CustomerNew.vue
@@ -1,10 +1,19 @@
 <template>
-<h3>Form</h3>
+    <v-form>
+      <v-text-field label="Name" type="text"></v-text-field>
+      <v-text-field label="Purchases" type="number"></v-text-field>
+      <v-btn type="submit" color="primary">Submit</v-btn>
+    </v-form>
 </template>
 
 <script>
 export default {
-  name: 'CustomerNew'
+  name: 'CustomerNew',
+  data: function () {
+    return {
+      customer: {}
+    }
+  }
 }
 </script>
 
