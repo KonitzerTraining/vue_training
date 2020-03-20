9138e87a4d1e9713d97db9f8931144c3c606c073
KonitzerTraining <mk@konitzer-training.de>
2020-03-20 13:18:32 +0100

    Vue Instance

--- 04_vue_ts/src/store/index.ts
+++ 04_vue_ts/src/store/index.ts
@@ -1,16 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import customerService from '@/features/customersFeature/services/customerService'
+import ICustomer from '@/features/customersFeature/model/ICustomer'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     count: 0,
-    customers: []
+    customers: [] as ICustomer[]
   },
   mutations: {
-    SET_CUSTOMERS (state, customers) {
+    SET_CUSTOMERS (state, customers: ICustomer[]) {
       state.customers = customers
     }
   },
